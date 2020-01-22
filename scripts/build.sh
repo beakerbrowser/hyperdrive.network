e37c78cdee3fc32a2b0f77c4b4c7d9cf519a1196
Paul Frazee <pfrazee@gmail.com>
2020-01-22 14:10:55 -0600

    Build tooling fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,5 @@ cp -R ./js ./build/js
 cp -R ./css ./build/css
 cp -R ./webfonts ./build/webfonts
 cp ./favicon.png ./build/favicon.png
-rollup ./js/main.js --format iife --name main --file ./build/js/main.js
+rollup ./js/main.js --format iife --name main --file ./build/js/main.build.js
 cat ./index.html | sed 's/main.js/main.build.js/' > ./build/index.html
